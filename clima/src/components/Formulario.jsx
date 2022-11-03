d94335fcff24ebd20567ea32fc68ed45c31331ea
mauricio jaramillo <106204814+mauricio1108@users.noreply.github.com>
2022-11-03 16:20:09 -0500

    se consume la api por medio del link de long,lat

--- clima/src/components/Formulario.jsx
+++ clima/src/components/Formulario.jsx
@@ -1,43 +1,62 @@
 import useClima from "../hooks/useClima";
+import{useState} from 'react';
 
 const Formulario = () => {
 
-    const {busqueda, datosBusqueda} = useClima();
+    const [alerta, setAlerta] = useState('');
 
-     const {ciudad, pais} = busqueda;
+    const {busqueda, datosBusqueda, consultarClima} = useClima();
+
+    const {ciudad, pais} = busqueda; 
+ 
+    const handlesubmit = (e) => {
+       e.preventDefault();
+        if (Object.values(busqueda).includes('')) {
+            setAlerta('Todos los campos son obligatorios');
+            return;
+        }
+
+        consultarClima(busqueda);
+    }       
+ 
+    
   return (
     <>
       <div className="contenedor">
-        <div className="campo">
-        <label htmlFor="ciudad">Ciudad: </label>
-        <input 
-        type="text"
-         id="ciudad" 
-         name="ciudad" 
-            onChange={datosBusqueda} 
-            value={ciudad}
-        />
-        </div>
 
-        <div className="campo">
-          <label htmlFor="pais">pais: </label>
-          <select id="pais"
-            name="pais"
-            onChange={datosBusqueda}
-            value={pais}
-          >
-            <option value=""> Seleccione un país </option>
-            <option value="US">Estados Unidos</option>
-            <option value="MX">México</option>
-            <option value="AR">Argentina</option>
-            <option value="CO">Colombia</option>
-            <option value="CR">Costa Rica</option>
-            <option value="ES">España</option>
-            <option value="PE">Perú</option>
-          </select>
-        </div>
+        {alerta && <p className="alerta">{alerta}</p>}
+        <form onSubmit={handlesubmit}>
+          <div className="campo">
+            <label htmlFor="ciudad">Ciudad: </label>
+            <input
+              type="text"
+              id="ciudad"
+              name="ciudad"
+              onChange={datosBusqueda}
+              value={ciudad}
+            />
+          </div>
 
-        <input type="submit" value="Buscar Clima" className="btn btn-primario btn-block" />
+          <div className="campo">
+            <label htmlFor="pais">pais: </label>
+            <select id="pais" name="pais" onChange={datosBusqueda} value={pais}>
+              <option value=""> Seleccione un país </option>
+              <option value="US">Estados Unidos</option>
+              <option value="MX">México</option>
+              <option value="AR">Argentina</option>
+              <option value="CO">Colombia</option>
+              <option value="CR">Costa Rica</option>
+              <option value="ES">España</option>
+              <option value="PE">Perú</option>
+            </select>
+          </div>
+
+          <input
+            type="submit"
+            value="Buscar Clima"
+            className="btn btn-primario btn-block"
+          />
+        </form>
       </div>
     </>
   );
--- clima/src/context/ClimaProvaider.jsx
+++ clima/src/context/ClimaProvaider.jsx
@@ -1,12 +1,15 @@
 import {useState, createContext} from "react";  
+import axios from "axios";
 
 const ClimaContext = createContext();
 
 const ClimaProvaider = ({children}) => {
+
+    console.log (import.meta.env.VITE_API_KEY)
     const [busqueda, setBusqueda] = useState({
         ciudad: "",
         pais: "",
-        error: false,
+        
     });
 
     const datosBusqueda = (e) => {
@@ -15,9 +18,20 @@ const ClimaProvaider = ({children}) => {
             [e.target.name]: e.target.value,
         });
     };
+    
+    const consultarClima = async (datos) => {
+        try {
+             const {ciudad, pais} = datos;
+             const appId = import.meta.env.VITE_API_KEY;
+             const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${appId}`
+             console.log(url);
+        } catch (error) {
+            console.log (error)
+        }
+    }
 
     return (
-        <ClimaContext.Provider value={{busqueda, datosBusqueda}}>
+        <ClimaContext.Provider value={{busqueda, datosBusqueda, consultarClima}}>
             {children}
         </ClimaContext.Provider>
     )
